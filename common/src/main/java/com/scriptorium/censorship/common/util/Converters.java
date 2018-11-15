b0725a5df298767161262cd90a2e9b7372dcbeef
Taras Tkachenko <TarasTkachenko@me.com>
2018-11-16 00:33:20 +0200

    Add receiving all misspelled forms of the word

--- common/src/main/java/com/scriptorium/censorship/common/util/Converters.java
+++ common/src/main/java/com/scriptorium/censorship/common/util/Converters.java
@@ -2,6 +2,7 @@ package com.scriptorium.censorship.common.util;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.*;
 
 import static java.lang.Integer.parseInt;
 
@@ -10,7 +11,8 @@ import static java.lang.Integer.parseInt;
  */
 
 public final class Converters {
-    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("d MMM yyyy HH:mm");
+    private static final DateTimeFormatter DATE_TIME_FORMATTER =
+            DateTimeFormatter.ofPattern("d MMM yyyy HH:mm", Locale.getDefault());
 
     private Converters() { }
 
@@ -26,5 +28,79 @@ public final class Converters {
 
     public static String toString(LocalDateTime ldt) {
         return ldt == null ? "" : ldt.format(DATE_TIME_FORMATTER);
+    }
+
+    public enum CHAR_FORM {
+        YI_Y,
+        E_YE_EE_YO
+    }
+
+    private static final List<Character> yi_type = new ArrayList<>(3);
+    private static final List<Character> ye_type = new ArrayList<>(4);
+
+    static {
+        yi_type.add('И');
+        yi_type.add('І');
+        yi_type.add('Ы');
+
+        ye_type.add('Е');
+        ye_type.add('Є');
+        ye_type.add('Э');
+        ye_type.add('Ё');
+    }
+
+    public static Set<String> getAllWordVariants(String word) {
+        String initialForm = word.toUpperCase();
+        Set<String> resultSet = new HashSet<>();
+        resultSet.add(initialForm);
+
+        Map<Integer, CHAR_FORM> map = composePositions(initialForm);
+
+        generateWordSet(initialForm, map, resultSet);
+
+        return resultSet;
+    }
+
+    private static void generateWordSet(String currentWord, Map<Integer, CHAR_FORM> map, Set<String> resultSet) {
+        for (Map.Entry<Integer, CHAR_FORM> entry : map.entrySet()) {
+            Set<String> allFormsAtIndex = getAllFormsAtIndex(currentWord, entry.getKey(), entry.getValue());
+            resultSet.addAll(allFormsAtIndex);
+            Map<Integer, CHAR_FORM> newMap = new HashMap<>(map);
+            newMap.remove(entry.getKey());
+            if (newMap.isEmpty()) {
+                return;
+            }
+            allFormsAtIndex.forEach(s -> generateWordSet(s, newMap, resultSet));
+        }
+    }
+
+    private static Map<Integer, CHAR_FORM> composePositions(String initialWord) {
+        Map<Integer, CHAR_FORM> map = new HashMap<>();
+        for (int idx = 0; idx < initialWord.length(); idx++) {
+            char charAt = initialWord.charAt(idx);
+            if (yi_type.contains(charAt)) {
+                map.put(idx, CHAR_FORM.YI_Y);
+            } else if (ye_type.contains(charAt)) {
+                map.put(idx, CHAR_FORM.E_YE_EE_YO);
+            }
+        }
+        return map;
+    }
+
+    private static Set<String> getAllFormsAtIndex(String initialForm, int index, CHAR_FORM char_form) {
+        StringBuffer constructedWord = new StringBuffer(initialForm);
+        Set<String> resultSet = new HashSet<>(4);
+        if (CHAR_FORM.YI_Y == char_form) {
+            yi_type.forEach(character -> addModifiedWord(index, character, constructedWord, resultSet));
+        } else if (CHAR_FORM.E_YE_EE_YO == char_form) {
+            ye_type.forEach(character -> addModifiedWord(index, character, constructedWord, resultSet));
+        }
+
+        return resultSet;
+    }
+
+    private static void addModifiedWord(int index, Character character, StringBuffer constructedWord, Set<String> resultSet) {
+        constructedWord.setCharAt(index, character);
+        resultSet.add(constructedWord.toString());
     }
 }
---
+++ common/src/test/java/com/scriptorium/censorship/common/util/ConvertersTest.java
@@ -0,0 +1,68 @@
+package com.scriptorium.censorship.common.util;
+
+import org.junit.Test;
+
+import java.util.Set;
+
+import static com.scriptorium.censorship.common.util.Converters.getAllWordVariants;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by taras on 2018-11-15.
+ */
+
+public class ConvertersTest {
+    private static final String simpleWord = "Право";
+    private static final String wordWith2Variants = "Приво";
+
+    @Test
+    public void getSimpleWordWithoutVariants() {
+        Set<String> resultSet = getAllWordVariants(simpleWord);
+        assertEquals(1, resultSet.size());
+        assertTrue(resultSet.contains(simpleWord.toUpperCase())) ;
+    }
+
+    @Test
+    public void getWordWith3Variants() {
+        Set<String> resultSet = getAllWordVariants(wordWith2Variants);
+        assertEquals(3, resultSet.size());
+        assertTrue(resultSet.contains(wordWith2Variants.toUpperCase())) ;
+        assertTrue(resultSet.contains("ПРІВО")) ;
+    }
+
+    @Test
+    public void getTippingVariants() {
+        Set<String> resultSet = getAllWordVariants("Типпинг");
+        assertTrue(resultSet.contains("ТИППИНГ")) ;
+        assertTrue(resultSet.contains("ТІППИНГ")) ;
+        assertTrue(resultSet.contains("ТІППІНГ")) ;
+        assertTrue(resultSet.contains("ТИППІНГ")) ;
+        assertTrue(resultSet.contains("ТЫППІНГ")) ;
+    }
+
+    @Test
+    public void getComplexWord_1() {
+        Set<String> resultSet = getAllWordVariants("Типпінговиче");
+        assertEquals(108, resultSet.size());
+        assertTrue(resultSet.contains("ТИППІНГОВИЧЕ")) ;
+        assertTrue(resultSet.contains("ТИППИНГОВІЧЕ")) ;
+        assertTrue(resultSet.contains("ТИППИНГОВІЧЭ")) ;
+        assertTrue(resultSet.contains("ТИППИНГОВІЧЕ")) ;
+        assertTrue(resultSet.contains("ТИППИНГОВІЧЄ")) ;
+        assertTrue(resultSet.contains("ТИППИНГОВИЧЭ")) ;
+        assertTrue(resultSet.contains("ТИППИНГОВИЧЕ")) ;
+        assertTrue(resultSet.contains("ТИППИНГОВІЧЄ")) ;
+        assertTrue(resultSet.contains("ТИППІНГОВИЧЕ")) ;
+        assertTrue(resultSet.contains("ТІППІНГОВИЧЕ")) ;
+        assertTrue(resultSet.contains("ТІППИНГОВІЧЕ")) ;
+        assertTrue(resultSet.contains("ТІППИНГОВІЧЭ")) ;
+        assertTrue(resultSet.contains("ТІППИНГОВІЧЕ")) ;
+        assertTrue(resultSet.contains("ТІППИНГОВІЧЄ")) ;
+        assertTrue(resultSet.contains("ТІППИНГОВИЧЭ")) ;
+        assertTrue(resultSet.contains("ТІППИНГОВИЧЕ")) ;
+        assertTrue(resultSet.contains("ТІППИНГОВІЧЄ")) ;
+        assertTrue(resultSet.contains("ТІППІНГОВИЧЕ")) ;
+
+    }
+}
